62b0db74f2696bf154d9ca7181e72086a2b0ef7d
David Harris <dahveed15@gmail.com>
2018-06-04 11:23:37 -0700

    Create reducers

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,14 +1,13 @@
 import { combineReducers } from 'redux';
 
-// import errorsReducer from './errors_reducer';
-// import sessionReducer from './session_reducer';
-// import AlbumsReducer from './album_reducer';
-// import PhotosReducer from './photo_reducer';
-// import CommentsReducer from './comment_reducer';
-// import ExplorePhotosReducer from './explore_photo_reducer';
+import entities from './entities_reducer';
+import session from './session_reducer';
+import errors from './errors_reducer';
 
 const rootReducer = combineReducers({
-
+  entities,
+  session,
+  errors,
 });
 
 export default rootReducer;
