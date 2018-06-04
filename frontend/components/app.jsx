8f742b707f6a8c852e7f63568f707094ebddd350
David Harris <dahveed15@gmail.com>
2018-06-04 11:46:11 -0700

    Make changes

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -8,13 +8,23 @@ import {
   HashRouter,
   Refresh
 } from 'react-router-dom';
-
+import NavBarContainer from './nav/nav_bar_container';
+import HomepageContainer from './homepage/homepage_container';
+import Footer from './nav/footer';
+import LoginContainer from './session/form_container';
 
 const App = (store) => {
   return (
-    <div>
-      <h1>Hi there!</h1>
+    <div className="container">
+      <Route path="/" component={HomepageContainer} />
     </div>
   );
 };
 export default App;
+
+
+// <Route path="/" component={NavBarContainer} />
+// <Switch>
+//   <AuthRoute exact path="/" component={HomepageContainer} />
+// </Switch>
+// <Route path="/" component={Footer} />
--- frontend/components/homepage/homepage.jsx
+++ frontend/components/homepage/homepage.jsx
@@ -10,39 +10,39 @@ class Homepage extends React.Component {
     super(props);
   }
 
-  componentDidMount() {
-    const animationFadeIn = ['animated', 'fadeInUp'];
-    const animationFadeOut = ['animated', 'fadeOutUp'];
-    const animationAppear = ['animated', 'fadeIn'];
-    const animationPulse = ['animated', 'pulse'];
-    const animationEnd = 'webkitAnimationEnd mozAnimationEnd oAnimationEnd animationend';
-
-    const writeLess = document.getElementById('write-less');
-    const rememberMore = document.getElementById('remember-more');
-    const writeAway = document.getElementById('write-away');
-    const journaling = document.getElementById('journaling');
-
-    writeLess.classList.add(...animationFadeIn).addEventListener(animationEnd, function(event1) {
-      writeLess.classList.remove(...animationFadeIn);
-      writeLess.classList.add(...animationFadeOut);
-
-      rememberMore.classList.add(...animationFadeIn).addEventListener(animationEnd, function(event2) {
-        rememberMore.classList.remove(...animationFadeIn);
-        rememberMore.classList.add(...animationFadeOut);
-
-        writeAway.classList.add(...animationFadeIn).addEventListener(animationEnd, function(event3) {
-          journaling.classList.add(...animationAppear).addEventListener(animationEnd, function(event4) {
-            journaling.classList.remove(...animationAppear);
-            journaling.classList.add(...animationPulse);
-            event4.target.removeEventListener(event4.type, arguments.callee);
-          });
-          event3.target.removeEventListener(event3.type, arguments.callee);
-        });
-        event2.target.removeEventListener(event2.type, arguments.callee);
-      });
-      event1.target.removeEventListener(event1.type, arguments.callee);
-    });
-  }
+  // componentDidMount() {
+  //   const animationFadeIn = ['animated', 'fadeInUp'];
+  //   const animationFadeOut = ['animated', 'fadeOutUp'];
+  //   const animationAppear = ['animated', 'fadeIn'];
+  //   const animationPulse = ['animated', 'pulse'];
+  //   const animationEnd = 'webkitAnimationEnd mozAnimationEnd oAnimationEnd animationend';
+  //
+  //   const writeLess = document.getElementById('write-less');
+  //   const rememberMore = document.getElementById('remember-more');
+  //   const writeAway = document.getElementById('write-away');
+  //   const journaling = document.getElementById('journaling');
+  //
+  //   writeLess.classList.add(...animationFadeIn).addEventListener(animationEnd, function(event1) {
+  //     writeLess.classList.remove(...animationFadeIn);
+  //     writeLess.classList.add(...animationFadeOut);
+  //
+  //     rememberMore.classList.add(...animationFadeIn).addEventListener(animationEnd, function(event2) {
+  //       rememberMore.classList.remove(...animationFadeIn);
+  //       rememberMore.classList.add(...animationFadeOut);
+  //
+  //       writeAway.classList.add(...animationFadeIn).addEventListener(animationEnd, function(event3) {
+  //         journaling.classList.add(...animationAppear).addEventListener(animationEnd, function(event4) {
+  //           journaling.classList.remove(...animationAppear);
+  //           journaling.classList.add(...animationPulse);
+  //           event4.target.removeEventListener(event4.type, arguments.callee);
+  //         });
+  //         event3.target.removeEventListener(event3.type, arguments.callee);
+  //       });
+  //       event2.target.removeEventListener(event2.type, arguments.callee);
+  //     });
+  //     event1.target.removeEventListener(event1.type, arguments.callee);
+  //   });
+  // }
 
   render() {
     return (
