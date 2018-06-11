b33482e7a1d34fe86048469b4bc32e34ca40756a
ntuzer <rgjr22@gmail.com>
2018-06-11 15:41:36 -0700

    Add changes

--- frontend/components/session/login_form_container.js
+++ frontend/components/session/login_form_container.js
@@ -4,17 +4,15 @@ import { Link } from 'react-router-dom';
 import { login } from '../../actions/session_actions';
 import LoginForm from './login_form';
 
-const mapStateToProps = ({ errors }) => {
-  return {
+const mapStateToProps = ({ errors }) => ({
     errors: errors.session,
     formType: 'login',
-  };
-};
+  }
+);
 
-const mapDispatchToProps = dispatch => {
-  return {
+const mapDispatchToProps = dispatch => ({
     processForm: (email) => dispatch(login(email)),
-  };
-};
+  }
+);
 
 export default connect(mapStateToProps, mapDispatchToProps)(LoginForm);
